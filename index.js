9a25709661c7f95c301b8efa40e28bba5c7b2ef9
mark novelero <mark.novelero@gmail.com>
2021-04-05 11:06:38 -0400

    commit tree 8fb4940e9dc3c9c0b3705331281adc5dbb7a81a2

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser= 'MARK NOVELERO';
+const welcomeMessage= 'Welcome to Flatbook, ' + currentUser + '!'; 
+const excitedWelcomeMessage= 'WELCOME TO FLATBOOK, ' + currentUser + '!';
+const shortGreeting= 'Welcome, ' + 'M!';
